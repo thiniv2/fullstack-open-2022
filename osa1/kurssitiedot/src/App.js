bd3d979aa96fc6e72a4490732a03923752528653
thiniv2 <nthien098@gmail.com>
2022-03-08 17:52:03 +0200

    part1 osa1 & osa2

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,56 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      {props.part}: {props.exercise}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercise={props.exercises[0]} />
+      <Part part={props.part[1]} exercise={props.exercises[1]} />
+      <Part part={props.part[2]} exercise={props.exercises[2]} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises: {props.total}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 74
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
